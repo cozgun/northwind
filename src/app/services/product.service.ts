fceb5877ec7106b86f551d12503b724a1bd48160
cozgun <59412630+cozgun@users.noreply.github.com>
2021-04-06 21:49:24 +0300

    20 gün ders sonu

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../Models/listResponseModel';
 import { Product } from '../Models/product';
+import { ResponseModel } from '../Models/responseModel';
 
 @Injectable({
   providedIn: 'root',
@@ -20,4 +21,9 @@ export class ProductService {
     let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
+
+  add(product:Product): Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add",product)
+  }
+
 }
